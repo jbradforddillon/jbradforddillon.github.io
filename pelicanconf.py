0f33c4906b4f8f2a9977fa68bdacbd67f33a30a6
Brad Dillon <jbradforddillon@gmail.com>
2013-05-24 23:28:40 -0400

    Initial source commit

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Brad Dillon'
+SITENAME = u'Just call me Brad'
+TAGLINE = u'Husband, father, iOS Developer at Double Encore, amateur yo-yoer, occasional game tinkerer, audiobook enthusiast. You can just call me Brad.'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Double Encore', 'http://doubleencore.com/'),
+          ('Twitter', 'http://twitter.com/jbradforddillon'))
+
+# Social widget
+SOCIAL = ()
+
+# Logo
+USER_LOGO_URL = 'http://jbradforddillon.com/wp/wp-content/uploads/2012/08/Avatar-300x300.jpeg'
+
+DEFAULT_PAGINATION = False
+
+THEME = 'themes/svbhack'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
